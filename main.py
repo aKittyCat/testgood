856f3bdf6d9d46b34268773739c1fa852ec2786d
aKittyCat <87542711+aKittyCat@users.noreply.github.com>
2022-06-27 09:50:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ for filename in os.listdir('./commands'):
   if filename.endswith('.py'):
     client.load_extension(f'commands.{filename[:-3]}')  
   
-TOKEN=config('TOKEN')
-client.run(TOKEN)
+TOKEN=config('OTkwNzQzODUwODE1NDYzNDg0.GC8N6f.eofE1v7_FbfMuJ2knC0dn5GlQxivg69IklX4ps')
+client.run(OTkwNzQzODUwODE1NDYzNDg0.GC8N6f.eofE1v7_FbfMuJ2knC0dn5GlQxivg69IklX4ps)
